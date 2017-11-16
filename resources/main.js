2ecb29014985a3256de15589018f65a6926a5268
AK Krajewska <akrajewska@gmail.com>
2017-11-15 16:02:04 -0800

    minimum viable calculator

--- resources/main.js
+++ resources/main.js
@@ -1,1 +1,21 @@
-var box = 
+var box = document.getElementById('display');
+
+function addToScreen(x){
+    box.value += x;
+    if(x === 'ac'){
+        box.value = '';
+    }  
+}
+
+function answer(){
+    x = box.value;
+    x = eval(x); //built in javascript function that evaluates string as a number it's neat as heck
+    box.value = x;
+    }
+
+function backspace(){
+    var num = box.value;
+    var len = num.length-1;
+    var newNum = num.substring(0, len);
+    box.value = newNum;
+}
