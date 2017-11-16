f6c0347f7252045e052c9ae04786dfb4b68d506a
AK Krajewska <akrajewska@gmail.com>
2017-11-15 17:00:44 -0800

    planned improvements in comments

--- resources/main.js
+++ resources/main.js
@@ -1,6 +1,10 @@
 var box = document.getElementById('display');
 
 function addToScreen(x){
+    //add validation
+    //allow math operator only after an integer
+    //allow only one decimal point per chunk of integers
+    
     box.value += x;
     if(x === 'ac'){
         box.value = '';
@@ -11,6 +15,7 @@ function answer(){
     x = box.value;
     x = eval(x); //built in javascript function that evaluates string as a number it's neat as heck
     box.value = x;
+    //probably add some rounding
     }
 
 function backspace(){
