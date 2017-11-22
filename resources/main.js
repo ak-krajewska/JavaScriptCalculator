8043a1bd4657ea9f06fc88c0ba3cc7a643eb02aa
AK Krajewska <akrajewska@gmail.com>
2017-11-21 16:39:55 -0800

    cleanup comments, usless files

--- resources/main.js
+++ resources/main.js
@@ -1,29 +1,16 @@
-/* notes for later use
-///How to Parse a String by Operator
-var regex = /[\+/*-]/;
-var str = "1+2-3*4/5";
-console.log(str.split(regex));
-///things that need work
-honestly this whole thing should probably be done with arrays much as I tried to avoid it by carrying out mathematical operations in the display box. Very sadface.
-*/
-
+//global variables
 var box = document.getElementById('display');
-
 var allowedNums = ["0","1","2","3","4","5","6","7","8","9"];
-var allowedInts = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var allowedOperators = ["+", "-", "*", "/"];
 var hasDecimal = false; //toggle for tracking if a chunk has a decimal
 
 
 //I'd like to allow one leading zero but only if it is followed by a period
 
-function addToScreen(x){
-    //add validation
-    //allow math operator only after an integer
-       
+function addToScreen(x){    
     //you want to enter a number
     if (allowedNums.indexOf(x) != -1){
-                console.log("It's a number, so you can add it");
+                console.log("It's a number, so you can enter it");
                 //no leading zero, no zero directly after an operator
                 if (((x == 0) && (box.value.length == 0)) || (x == 0) && (allowedOperators.indexOf(box.value.charAt(box.value.length - 1)) != -1) ){
                     alert("no leading zero");
@@ -31,12 +18,7 @@ function addToScreen(x){
                     box.value += x;
             }
     
-    
-    //paul's suggestion: track if you've added a decimal, and don't allow a second decimal if the flag says that a decimal has been added. A math operator clears the flag.
-    //ok but what if someone goes AC? 
-    //how about we clear the flag after the first number after the math operator
-    
-    // //allow only one decimal point per chunk of integers
+    //allow only one decimal point per chunk of numerals
     //you want to enter a decimal point
     if (x == "."){
         if (hasDecimal == true){
@@ -63,7 +45,6 @@ function addToScreen(x){
     }
     
     
-    
     if(x === 'ac'){
         hasDecimal = false;
         box.value = '';
@@ -79,7 +60,7 @@ function answer(){
     
     
     x = box.value;
-    x = eval(x); //built in javascript function that evaluates string as a number it's neat as heck
+    x = eval(x); //built in javascript function that evaluates string as a number
     box.value = x;
     hasDecimal = false;
     //probably add some rounding
@@ -88,39 +69,33 @@ function answer(){
 function backspace(){
     var num = box.value;
     var len = num.length-1;
+    //untoggle the decimal flag if what your're deleting is a decimal
     if (num.charAt(len) == "."){
         console.log("deleteing a decimal");
         hasDecimal = false;
         }
     
     var newNum = num.substring(0, len);
-    //untoggle the decimal flag if what your're deleting is a decimal
     
-    //if you delete an operator, check if the next "chunk" has a decimal and reset the flag to true
+    //if you delete an operator, check if the next chunk has a decimal and if yes reset the flag to true
     if (allowedOperators.indexOf(num.charAt(len)) != -1){ 
         console.log("deleting an operator");
-        //look if I just prevented them from adding the duplicative decimal in the first place, I wouldn't need to do this check. or wait. no, damn it. I still have to check as I type
+        
         //set flag to false
         hasDecimal = false;
         
         //parse the remaining string
-        var regex = /[\+*/-]/;
+        var regex = /[\+*/-]/; //regex for operators
         console.log("newNum is " + newNum)
         var parsedArr = newNum.split(regex);
-        console.log(parsedArr); 
         console.log("the last item is " + parsedArr[parsedArr.length -1]);
         
         //evaluate the last item in the array for a decimal
         if (parsedArr[parsedArr.length -1].indexOf(".") !== -1){
             console.log(parsedArr[parsedArr.length -1] + " contains a decimal");
             //if the last item in the array has a decimal, set the flag to true
-            hasDecimal = true;
-            
+            hasDecimal = true; 
         }
-        
-        
-        
     }
-    
     box.value = newNum;
 }
